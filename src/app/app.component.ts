51b9bbbbe287de2bcf6a8ace676c4e8d37f7ba79
chaitanya <[parandkarchaitanya@gmail.com]>
2025-02-19 18:06:51 +0530

    Adding new project called Employee database

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { EmployeeInfoComponent } from '../component/employee-info/employee-info.component';
+import { EmployeeListComponent } from '../component/employee-list/employee-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, EmployeeInfoComponent, EmployeeListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/model/employee_data.model.ts
@@ -0,0 +1,23 @@
+export interface employeeData{
+
+    firstName:string,
+    lastName:string,
+    age:number,
+    dob:string,
+    gender:string,
+    email:string,
+    mobileNumber:number
+
+}
+
+/*
+ {
+        "firstName":"Chaitanya",
+        "lastName":"P",
+        "age":24,
+        "dob":"25/03/2000",
+        "gender":"male",
+        "email":"abc@gmail.com",
+        "mobileNumber":1234567
+    }
+*/
